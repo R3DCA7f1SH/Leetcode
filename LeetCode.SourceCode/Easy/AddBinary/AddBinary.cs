74842ad652630455abf6ac1d4c10a1d1619b8c3f
R3DCA7f1SH <R3DCA7f1SH@mail.ru>
2024-03-03 13:13:57 +0300

    AddBinary

---
+++ LeetCode.SourceCode/Easy/AddBinary/AddBinary.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LeetCode.SourceCode.Easy
+{
+    public class AddBinarySolution
+    {
+        private int CharToInt(char ch) => ch - '0';
+
+        public string AddBinary(string a, string b) 
+        { 
+            var aLength = a.Length - 1;
+            var bLength = b.Length - 1;
+            var stack = new Stack<int>();
+            var carry = 0;
+            while (aLength >= 0 || bLength >= 0)
+            {
+                var aValue = 0;
+                var bValue = 0;
+                if (aLength >= 0) aValue = CharToInt(a[aLength--]);
+                if (bLength >= 0) bValue = CharToInt(b[bLength--]);
+                var sum = carry + aValue + bValue;
+                carry = sum / 2;
+                var digit = sum % 2;
+                stack.Push(digit);
+            }
+            if (carry > 0) stack.Push(carry);
+            var result = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                result.Append(stack.Peek());
+                stack.Pop();
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/AddBinary/AddBinary.Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Tests.Easy
+{
+    using LeetCode.SourceCode.Easy;
+    public class AddBinaryTest
+    {
+        private readonly AddBinarySolution solution = new();
+        [Fact]
+        public void TestIsValid1()
+        {
+            Assert.Equal(solution.AddBinary("11", "1"), "100");
+        }
+        [Fact]
+        public void TestIsValid2()
+        {
+            Assert.Equal(solution.AddBinary("1010", "1011"), "10101");
+        }
+    }
+}

03651f5b9ff361ede52dfc44d75fa4251cd620b5
R3DCA7f1SH <R3DCA7f1SH@mail.ru>
2024-03-03 16:43:47 +0300

    PlusOne

---
+++ LeetCode.SourceCode/Easy/PlusOne/PlusOne.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.SourceCode.Easy
+{
+    public class PlusOneSolution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var result = digits;
+            var length= digits.Length;
+            int toAdd = 1;
+            for(int i = length - 1; i >= -1; i--)
+            {
+                if (toAdd == 0) break;
+                var sum = result[i] + toAdd;
+                result[i] = sum % 10;
+                toAdd = sum / 10;
+                if (i == 0)
+                {
+                    if (toAdd != 0)
+                    {
+                        Array.Resize(ref result, ++length); // increase array length by 1
+                        Array.Copy(result, 0, result, 1, length - 1);
+                        result[i] = toAdd;
+                    }
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/PlusOne/PlusOne.Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.SourceCode.Easy;
+
+namespace LeetCode.Tests.Easy
+{
+    public class PlusOneTests
+    {
+        private readonly PlusOneSolution solution = new();
+        
+        [Fact]
+        public void TestIsValid1()
+        {
+            Assert.Equal(solution.PlusOne([1, 2, 3]), [1, 2, 4]);
+        }
+
+        [Fact]
+        public void TestIsValid2()
+        {
+            Assert.Equal(solution.PlusOne([4, 3, 2, 1]), [4, 3, 2, 2]);
+        }
+
+        [Fact]
+        public void TestIsValid3()
+        {
+            Assert.Equal(solution.PlusOne([9]), [1,0]);
+        }
+    }
+}
